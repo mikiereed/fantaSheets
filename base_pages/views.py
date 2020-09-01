a9bd91ef2c1c371564ba9facc9b53325e61b392a
mikie Reed <mikie@prenotions.com>
2020-08-31 19:47:04 -0700

    template base, index, about

--- base_pages/views.py
+++ base_pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'base_pages/index.html')
+
+def about(request):
+    return render(request, 'base_pages/about.html')
