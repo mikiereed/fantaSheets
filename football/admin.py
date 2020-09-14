3641c43917df1eb8597612c1fcb3a80faf0a41d3
mikie Reed <mikie@prenotions.com>
2020-09-14 15:06:32 -0700

    admin settings football league settings

--- football/admin.py
+++ football/admin.py
@@ -4,4 +4,9 @@ from .models import LeagueSettings
 
 @admin.register(LeagueSettings)
 class LeagueSettingsAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'title', 'league_hosting_site')
+    list_display = ('id', 'owner', 'title', 'league_hosting_site')
+    list_display_links = ('id', 'title')
+    list_filter = ('owner', 'league_hosting_site')
+    search_fields = ('owner', 'title')
+    list_per_page = 100
+    # list_editable = ('league_hosting_site',)
