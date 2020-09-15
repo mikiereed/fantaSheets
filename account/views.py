3042fce72eab034a29a92a1f0f4e5f0dfa18828c
mikie Reed <mikie@prenotions.com>
2020-09-15 14:55:21 -0700

    add messages to login and register

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from .forms import UserRegistrationForm, UserEditForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def register(request):
