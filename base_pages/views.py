b3d05ce6fda301ee8701eb6897a7e68dfa6c1968
mikie Reed <mikie@prenotions.com>
2020-10-12 15:15:09 -0700

    clean up landing page

--- base_pages/views.py
+++ base_pages/views.py
@@ -3,10 +3,22 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from football.models import LeagueSettings as FootballLeagueSettings
-
+from operator import attrgetter
 
 def index(request):
-    return render(request, 'base_pages/index.html')
+    
+    # league_settings = FootballLeagueSettings.objects.filter(owner=request.user)
+    all_sheets = FootballLeagueSettings.objects.all()
+
+    sorted_sheets = sorted(all_sheets, key= attrgetter('id'))
+
+    first_3_sheets = sorted_sheets[:3]
+
+    context = {
+        'default_sheets': first_3_sheets
+    }
+
+    return render(request, 'base_pages/index.html', context)
 
 
 def about(request):
@@ -15,6 +27,7 @@ def about(request):
 
 @login_required
 def dashboard(request):
+
     league_settings = FootballLeagueSettings.objects.filter(owner=request.user)
 
     context = {
